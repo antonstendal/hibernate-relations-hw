248e5c7d17a133576cb5131615032a0b406c3a8d
hero <antonstendal19@gmail.com>
2026-03-23 06:17:46 +0100

    Add Hibernate configuration and basic persistence for Movie, Actor and Country entities

--- src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/ActorServiceImpl.java
@@ -1,16 +1,31 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import mate.academy.hibernate.relations.dao.ActorDao;
 import mate.academy.hibernate.relations.model.Actor;
 import mate.academy.hibernate.relations.service.ActorService;
+import mate.academy.hibernate.relations.util.DataProcessingException;
 
 public class ActorServiceImpl implements ActorService {
+    private final ActorDao actorDao;
+
+    public ActorServiceImpl(ActorDao actorDao) {
+        this.actorDao = actorDao;
+    }
+
     @Override
     public Actor add(Actor actor) {
-        return null;
+        if (actor == null) {
+            throw new DataProcessingException("Actor can't be null");
+        }
+        return actorDao.add(actor);
     }
 
     @Override
     public Actor get(Long id) {
-        return null;
+        if (id == null) {
+            throw new DataProcessingException("Id can't be null");
+        }
+        return actorDao.get(id).orElseThrow(
+                () -> new DataProcessingException("Can't find actor by id " + id));
     }
 }
--- src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
+++ src/main/java/mate/academy/hibernate/relations/service/impl/MovieServiceImpl.java
@@ -1,16 +1,31 @@
 package mate.academy.hibernate.relations.service.impl;
 
+import mate.academy.hibernate.relations.dao.MovieDao;
 import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.service.MovieService;
+import mate.academy.hibernate.relations.util.DataProcessingException;
 
 public class MovieServiceImpl implements MovieService {
+    private final MovieDao movieDao;
+
+    public MovieServiceImpl(MovieDao movieDao) {
+        this.movieDao = movieDao;
+    }
+
     @Override
     public Movie add(Movie movie) {
-        return null;
+        if (movie == null) {
+            throw new DataProcessingException("Movie can't be null");
+        }
+        return movieDao.add(movie);
     }
 
     @Override
     public Movie get(Long id) {
-        return null;
+        if (id == null) {
+            throw new DataProcessingException("Id can't be null");
+        }
+        return movieDao.get(id).orElseThrow(
+                () -> new DataProcessingException("Can't find movie by id " + id));
     }
 }
